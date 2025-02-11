c10de1385b9d30a0f8ac7f62cbc37caa68a91a1b
Utkarshh014 <shekharutkarsh75@gmail.com>
2025-02-11 09:34:48 +0530

    introduced js

---
+++ app.js
@@ -0,0 +1,8 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#resetbutton");
+let newGameBtn = document.querySelector("#newbutton");
+
+
+
+let turnO = true; //playerX, playerO
+let count = 0; //To Track Draw
